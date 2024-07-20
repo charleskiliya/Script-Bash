38d22c9c9f1a244f855ecce1c277ccc8c1a07fca
Charles kiliya <19km219@esisalama.org>
2024-07-20 12:07:07 +0200

    Ajout de couleur et du menu

--- Check.sh
+++ Check.sh
@@ -42,9 +42,55 @@ function kernel_check(){
     echo ""
 }
 
-function all_check(){
+function all_checks(){
     memory_check
     cpu_check
     tcp_check
     kernel_check
+}
+
+##
+# Color variables
+##
+green='\e[32m'
+blue='\e[34m'
+clear='\[0m'
+
+##
+# Color function
+##
+
+ColorGreen(){
+    echo -ne $green$clear
+}
+
+ColorBlue(){
+    echo -ne $blue$clear
+}
+
+
+menu(){
+    echo -ne "
+    
+    My first Menu
+
+    $(ColorGreen '1)') Memory Usage
+    $(ColorGreen '2)') CPU Load
+    $(ColorGreen '3)') Number of TCP Connections
+    $(ColorGreen '4)') Kernel version
+    $(ColorGreen '5)') All check
+    $(ColorGreen '0)') Exit
+    $(ColorBlue 'Choose an option')"
+            read a 
+            case $a in
+                    1) memory_check; menu;;
+                    2) cpu_check; menu;;
+                    3) tcp_check; menu;;
+                    4) kernel_check; menu;;
+                    5) all_checks; menu;;
+                            0) exit 0;;
+                            *) echo -e $red "Wrong Option. "$clear;
+    WrongCommand;;
+            esac
+
 }
