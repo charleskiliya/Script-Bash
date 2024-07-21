07c75ef25d5be1e0b0f31ba134e36efb400c2456
Charles kiliya <19km219@esisalama.org>
2024-07-21 07:15:43 +0200

    Voici la version finale du script

--- Check.sh
+++ Check.sh
@@ -12,7 +12,7 @@ Server_name=$(hostname)
 
 function memory_check(){
     echo ""
-        echo "The memory usage on ${server_name} is:  "
+        echo "The memory usage on ${Server_name} is:  "
         free -h
         echo ""
 
@@ -20,7 +20,7 @@ function memory_check(){
 
 function cpu_check(){
     echo ""
-        echo "The CPU load on ${server_name} is: "
+        echo "The CPU load on ${Server_name} is: "
     echo ""
         uptime
     echo ""
@@ -28,7 +28,7 @@ function cpu_check(){
 
 function tcp_check(){
     echo ""
-        echo "The TCP connection on ${server_name} is: "
+        echo "The TCP connection on ${Server_name} is: "
     echo ""
         cat /proc/net/tcp | wc -l
     echo ""
@@ -36,7 +36,7 @@ function tcp_check(){
 
 function kernel_check(){
     echo ""
-        echo "The kernel version on  ${server_name} is: "
+        echo "The kernel version on  ${Server_name} is: "
         echo ""
         uname -r
     echo ""
@@ -54,43 +54,42 @@ function all_checks(){
 ##
 green='\e[32m'
 blue='\e[34m'
-clear='\[0m'
+clear='\e[0m'
 
 ##
 # Color function
 ##
 
 ColorGreen(){
-    echo -ne $green$clear
+    echo -ne $green$1$clear
 }
 
 ColorBlue(){
-    echo -ne $blue$clear
+    echo -ne $blue$1$clear
 }
-
 
 menu(){
-    echo -ne "
+echo -ne "
     
-    My first Menu
+My first Menu
 
-    $(ColorGreen '1)') Memory Usage
-    $(ColorGreen '2)') CPU Load
-    $(ColorGreen '3)') Number of TCP Connections
-    $(ColorGreen '4)') Kernel version
-    $(ColorGreen '5)') All check
-    $(ColorGreen '0)') Exit
-    $(ColorBlue 'Choose an option')"
-            read a 
-            case $a in
-                    1) memory_check; menu;;
-                    2) cpu_check; menu;;
-                    3) tcp_check; menu;;
-                    4) kernel_check; menu;;
-                    5) all_checks; menu;;
-                            0) exit 0;;
-                            *) echo -e $red "Wrong Option. "$clear;
-    WrongCommand;;
-            esac
-
+$(ColorGreen '1)') Memory Usage
+$(ColorGreen '2)') CPU Load
+$(ColorGreen '3)') Number of TCP Connections
+$(ColorGreen '4)') Kernel version
+$(ColorGreen '5)') All check
+$(ColorGreen '0)') Exit
+$(ColorBlue 'Choose an option : ') "
+        read a 
+        case $a in
+                1) memory_check; menu;;
+                2) cpu_check; menu;;
+                3) tcp_check; menu;;
+                4) kernel_check; menu;;
+                5) all_checks; menu;;
+                0) exit 0;;
+                *) echo -e $red "Wrong Option. "$clear;
+WrongCommand;;
+        esac
 }
+menu
